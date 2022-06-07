fc31d0436f8f8e8ce1788241162a44d0d0141f90
Walter Derezinski <walter@daringway.com>
2022-06-07 13:49:04 -0400

    Lab 2

--- main.tf
+++ main.tf
@@ -7,3 +7,43 @@ resource "aws_vpc" "main" {
     Name = "Walter VPC"
   }
 }
+
+resource "aws_subnet" "public-a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "walter-public-a"
+  }
+}
+
+resource "aws_subnet" "private-a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "walter-private-a"
+  }
+}
+
+resource "aws_subnet" "public-b" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "walter-public-b"
+  }
+}
+
+resource "aws_subnet" "private-b" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "walter-private-b"
+  }
+}
