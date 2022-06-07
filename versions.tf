ae688eec55a80bb7e9e5b87abc4e14fcb5f80437
Walter Derezinski <walter@daringway.com>
2022-06-07 13:06:59 -0400

    Lab 1

---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0, < 5.0"
+    }
+  }
+  required_version = ">= 1.1, < 1.3"
+}
+
+provider "aws" {
+  region = "us-east-2"
+  default_tags {
+    tags = {
+      Owner = "Walter"
+    }
+  }
+}
